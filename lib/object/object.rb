e9fa36d710ad897a90993d1c99740e8742f71d59
Mark Josef <McPhage@gmail.com>
2013-08-16 00:35:02 -0400

    Object#in? patch

--- lib/object/object.rb
+++ lib/object/object.rb
@@ -4,4 +4,13 @@ class ::Object
     self.class.new(*args, &block)
   end
 
+  def in?(container)
+    if container.respond_to? :cover?
+      container.cover?(self)
+    elsif container.respond_to? :include?
+      container.include?(self)
+    else
+      raise ArgumentError, "#{ container } does not respond to #cover? or #include?"
+    end
+  end
 end
